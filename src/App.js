59e3d7cf4d1ce04634a6c88aa3cc924fb2a7efc5
Tuan Anh Pham <tuananhpham568@gmail.com>
2023-04-25 22:34:42 +1000

    editted passives.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import "./App.scss";
 import Main from "./components/main";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "./state/userSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import React, { Component } from "react";
+import { PersistGate } from "redux-persist/integration/react";
+
+//Look into this more, essentailly saves redux state into localstorage to persist on refresh/reloads/window closes
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, userSlice);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 class App extends Component {
   render() {
     return (
-      // <React.StrictMode>
-      <div className="App">
-        <Main />
-      </div>
-      // </React.StrictMode>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistStore(store)}>
+          <div className="App">
+            <Main />
+          </div>
+        </PersistGate>
+      </Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,43 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./state/userSlice";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import reportWebVitals from "./reportWebVitals";
-import { PersistGate } from "redux-persist/integration/react";
 
-//Look into this more, essentailly saves redux state into localstorage to persist on refresh/reloads/window closes
-const persistConfig = { key: "root", storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, userSlice);
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistStore(store)}>
-      <App />
-    </PersistGate>
-  </Provider>
-);
+root.render(<App />);
 
 reportWebVitals();
