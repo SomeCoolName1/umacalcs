657e93ce7f4025b255d9ba164a7f5fcc6f001094
Tuan Anh Pham <tuananhpham568@gmail.com>
2023-04-25 22:58:21 +1000

    editted passives.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import Main from "./components/main";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./state/userSlice";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-import React, { Component } from "react";
-import { PersistGate } from "redux-persist/integration/react";
 
-//Look into this more, essentailly saves redux state into localstorage to persist on refresh/reloads/window closes
-const persistConfig = { key: "root", storage, version: 1 };
-const persistedReducer = persistReducer(persistConfig, userSlice);
+import React, { Component } from "react";
+
 const store = configureStore({
   reducer: userSlice,
 });
