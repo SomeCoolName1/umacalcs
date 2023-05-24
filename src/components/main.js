46bbabe1de342c81d4b91d0cf8d4cd737862b9b4
Tuan Anh Pham <tuananhpham568@gmail.com>
2023-05-24 19:29:33 +1000

    Added loading screen to display

--- src/components/main.js
+++ src/components/main.js
@@ -5,6 +5,7 @@ import Calculations from "./calculations/calculations";
 import Coursedetails from "./trackdetails/coursedetails";
 import Collapsible from "react-collapsible";
 import Racetrack from "./trackdetails/racetrack";
+import Loading from "./loading/loading";
 
 const initialStats = {
   speed: { en: "speed", jp: "スペード", value: 1000, adjusted: 0, final: 0 },
@@ -31,6 +32,9 @@ const Main = () => {
       <div className="race-course-container">
         <Coursedetails />
         <Racetrack stats={umaStats} />
+        <Loading />
+        This is the loading screen. It's not loading anything, it's just there
+        for fun
       </div>
     </div>
   );
