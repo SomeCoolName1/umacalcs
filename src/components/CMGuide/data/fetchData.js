76415d25c0517c4516ba96409b8fd165c5081afc
Tuan Anh Pham <tuananhpham568@gmail.com>
2023-10-29 20:20:16 +1100

    removed /api

--- src/components/CMGuide/data/fetchData.js
+++ src/components/CMGuide/data/fetchData.js
@@ -1,28 +1,26 @@
 export async function fetchSkills() {
-  const res = await fetch("https://" + process.env.VERCEL_URL + "/api/skills");
+  const res = await fetch("https://" + process.env.VERCEL_URL + "/skills");
 
   return await res.json();
 }
 
 export async function fetchCardRarityData() {
   const res = await fetch(
-    "https://" + process.env.VERCEL_URL + "/api/cardRarityData"
+    "https://" + process.env.VERCEL_URL + "/cardRarityData"
   );
 
   return await res.json();
 }
 
 export async function fetchSkillSet() {
-  const res = await fetch(
-    "https://" + process.env.VERCEL_URL + "/api/skillSet"
-  );
+  const res = await fetch("https://" + process.env.VERCEL_URL + "/skillSet");
 
   return await res.json();
 }
 
 export async function fetchSupportCard() {
   const res = await fetch(
-    "https://" + process.env.VERCEL_URL + "/api/supportCards"
+    "https://" + process.env.VERCEL_URL + "/supportCards"
   );
 
   return await res.json();
@@ -35,25 +33,25 @@ export async function fetchSupportCard() {
 //   fetchSupportCard: fetchSupportCard,
 // };
 // export async function fetchSkills() {
-//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/api/skills`);
+//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/skills`);
 
 //   return await res.json();
 // }
 
 // export async function fetchCardRarityData() {
-//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/api/cardRarityData`);
+//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/cardRarityData`);
 
 //   return await res.json();
 // }
 
 // export async function fetchSkillSet() {
-//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/api/skillSet`);
+//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/skillSet`);
 
 //   return await res.json();
 // }
 
 // export async function fetchSupportCard() {
-//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/api/supportCards`);
+//   const res = await fetch(`"https://" + process.env.VERCEL_URL + "/supportCards`);
 
 //   return await res.json();
 // }
