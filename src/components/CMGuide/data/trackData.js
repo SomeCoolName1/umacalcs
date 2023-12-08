5a76d059ec4a10c5140dce18dc1142bb00039e63
Tuan Anh Pham <tuananhpham568@gmail.com>
2023-12-08 12:38:17 +1100

    moved track data for easier file keeping

---
+++ src/components/CMGuide/data/trackData.js
@@ -0,0 +1,17 @@
+export const trackInfo = [
+  {
+    name: "RaceTrack",
+    detail: "Nakayama Inside Track (中山レース場)",
+    keyWord: "中山レース場○",
+  },
+  { name: "Distance", detail: "2500m", keyWord: "非根幹距離○" },
+  { name: "Season", detail: "Winter", keyWord: "冬ウマ娘○" },
+  { name: "Turn", detail: "右回り - Clockwise", keyWord: "右回り○" },
+  { name: "Ground", detail: "Heavy Weather", keyWord: "道悪○" },
+];
+
+export const trackTitle = {
+  name: "Champions Meeting",
+  date: "14/12 (dd/mm)",
+  details: "中山 芝 2500m（長距離） 右・内 冬 雪 重 昼",
+};
--- src/components/home/CMMain.js
+++ src/components/home/CMMain.js
@@ -5,22 +5,18 @@ import {
   fetchSkillSet,
   fetchSupportCard,
 } from "../CMGuide/data/fetchData";
-import DisplayInherit from "../CMGuide/getInherit";
-import DisplaySkill from "../CMGuide/getSkills";
 import "./CMMain.scss";
-import TrackInfo from "../CMGuide/getTrack";
 import { nigeSkills } from "../CMGuide/data/nigeSkills";
 import { senkouSkills } from "../CMGuide/data/senkouSkills";
 import { sashiSkills } from "../CMGuide/data/sashiSkills";
 import { oikomiSkills } from "../CMGuide/data/oikomiSkills";
 import { zenSkills } from "../CMGuide/data/zenSkills";
 import championBg from "../../assets/champions_bg.png";
-import recBg from "../../assets/champions_bg_2.png";
-import endBg from "../../assets/champions_bg_3.png";
 import raceCourseLogo from "../../assets/race_course_logo.png";
 import GuideTrack from "../CMGuide/guidesections/guideTrack";
 import GuideSkills from "../CMGuide/guidesections/guideSkills";
 import Loading from "./loading/loading";
+import { trackTitle, trackInfo } from "../CMGuide/data/trackData";
 
 const CMMain = () => {
   let [skillsData, setSkills] = useState(false);
@@ -39,18 +35,6 @@ const CMMain = () => {
     getSkillsData();
     let s = new Date().toLocaleString();
   }, []);
-
-  const trackInfo = [
-    {
-      name: "RaceTrack",
-      detail: "Nakayama Inside Track (中山レース場)",
-      keyWord: "中山レース場○",
-    },
-    { name: "Distance", detail: "2500m", keyWord: "非根幹距離○" },
-    { name: "Season", detail: "Winter", keyWord: "冬ウマ娘○" },
-    { name: "Turn", detail: "右回り - Clockwise", keyWord: "右回り○" },
-    { name: "Ground", detail: "Heavy Weather", keyWord: "道悪○" },
-  ];
 
   const {
     inherit: inhEveryone,
@@ -126,11 +110,11 @@ const CMMain = () => {
           <img src={raceCourseLogo} />
         </div>
         <div className="CMGuide-overview">
-          <p className="CMGuide-overview-title">Champions Meeting</p>
+          <p className="CMGuide-overview-title">{trackTitle.name}</p>
           <p>開催予定時期</p>
-          <p>14/12 (dd/mm)</p>
+          <p>{trackTitle.date}</p>
           <p>対象レース</p>
-          <p>中山 芝 2500m（長距離） 右・内 冬 雪 重 昼</p>
+          <p>{trackTitle.details}</p>
         </div>
       </div>
       <GuideTrack
